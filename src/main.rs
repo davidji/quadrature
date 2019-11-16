9d555c1e61bd2b6b6d8c4d2b7a632d43a0afc47b
David Ireland <davidji@pobox.com>
2019-11-16 14:20:51 +0000

    Using 0.5.0 instead of master avoids a panic. Set the right target for openocd. This version works as expected, echoing ascii bytes converted to upper case

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-//! examples/init.rs
 
 #![deny(unsafe_code)]
 #![deny(warnings)]
@@ -9,6 +8,7 @@ extern crate panic_semihosting;
 extern crate nb;
 
 use nb::Error::WouldBlock;
+use nb::block;
 
 use stm32f1xx_hal::{
     prelude::*,
@@ -28,7 +28,7 @@ type CommandSerial = Serial<CommandUsart, (PA2<Alternate<PushPull>>, PA3<Input<F
 type CommandTx = serial::Tx<CommandUsart>;
 type CommandRx = serial::Rx<CommandUsart>;
 
-#[rtfm::app(device = stm32f1::stm32f103, peripherals = true)]
+#[rtfm::app(device = stm32f1::stm32f103)]
 const APP: () = {
 
     struct Resources {
@@ -41,7 +41,9 @@ const APP: () = {
     fn init(_: init::Context) -> init::LateResources {
         rtfm::pend(stm32f103::Interrupt::USART1);
         let command_serial = command_serial();
-        let (tx, rx) = command_serial.split();
+        let (mut tx, mut rx) = command_serial.split();
+        command_write(&mut tx, "Hello\n");
+        rx.listen();
         init::LateResources {
             command_tx: tx,
             command_rx: rx,
@@ -49,7 +51,7 @@ const APP: () = {
     }
 
     #[task(binds = USART2, resources = [command_tx, command_rx])]
-    fn serial_interrupt(c: serial_interrupt::Context) {
+    fn command_serial_interrupt(c: command_serial_interrupt::Context) {
         command_poll(c.resources.command_tx, c.resources.command_rx);
     }
 
@@ -93,16 +95,29 @@ fn command_serial () -> CommandSerial {
 
 }
 
+fn command_write(tx : &mut CommandTx, s : & str) {
+    for c in s.chars() {
+        block!(tx.write(c as u8)).unwrap();
+    }
+}
+
 fn command_poll(tx : &mut CommandTx, rx : &mut CommandRx) {
-    loop {
+    'nonblockrx: loop {
         match rx.read() {
-            Ok(byte) => tx.write(byte).unwrap(),
+            Ok(byte) => block!(tx.write(to_upper(byte))).unwrap(),
             Err(err) => {
                 match err {
-                    WouldBlock => break,
+                    WouldBlock => break 'nonblockrx,
                     _ => panic!("Error reading from command serial"),
                 }
             },
         };
+    }
+}
+
+fn to_upper(byte: u8) -> u8 {
+    match byte as char {
+        'a'..='z' => return byte - ('a' as u8 - 'A' as u8),
+        _ => return byte
     }
 }
